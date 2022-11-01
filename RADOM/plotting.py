413f4f1b35fa590b2f6aae3f968c88ccf5883897
meichenfang <mffang.caltech@gmail.com>
2022-10-31 19:59:01 -0700

    more datasets

--- RADOM/plotting.py
+++ RADOM/plotting.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-cmps = ['viridis', 'plasma', 'inferno', 'magma', 'cividis']
+cmps = ['Purples', 'Blues', 'Greens', 'Oranges', 'Reds']
+colors = ['purple', 'blue', 'green', 'orange', 'r']
 colors20 = np.vstack((plt.cm.tab20b(np.linspace(0., 1, 20))[::2], plt.cm.tab20c(np.linspace(0, 1, 20))[1::2]))    
 
 def plot_t(weight,l=0,ax=None,t=None):
@@ -126,26 +127,26 @@ def plot_y(X,weight,traj,idx,gene_name=None,cell_colors=None):
     if len(idx)==1:
         if c==1:
             i=idx[0]
-            ax.scatter(t_hat,X[:,i,0],c=cell_colors);
-            ax.set_title(gene_name[0])
+            ax.scatter(t_hat,X[:,i,0],c='gray')#c=cell_colors);
+            ax.set_title(gene_name[i])
         else:
             i=idx[0]
-            ax[0].scatter(t_hat,X[:,i,0],c=cell_colors);
-            ax[0].set_title(gene_name[0]+" unspliced")
+            ax[0].scatter(t_hat,X[:,i,0],c='gray')#c=cell_colors);
+            ax[0].set_title(gene_name[i]+" unspliced")
             for ic in range(1,c):
-                ax[ic].scatter(t_hat,X[:,i,1],c=cell_colors);
-                ax[ic].set_title(gene_name[0]+" spliced")
+                ax[ic].scatter(t_hat,X[:,i,1],c='gray')#c=cell_colors);
+                ax[ic].set_title(gene_name[i]+" spliced")
 
     else:
         for i,j in enumerate(idx):
             if c==1:
-                ax[i].scatter(t_hat,X[:,j,0],c=cell_colors);
+                ax[i].scatter(t_hat,X[:,j,0],c='gray')#c=cell_colors);
                 ax[i].set_title(gene_name[j])
             else:
-                ax[0,i].scatter(t_hat,X[:,i,0],c=cell_colors);
+                ax[0,i].scatter(t_hat,X[:,i,0],c='gray')#c=cell_colors);
                 ax[0,i].set_title(gene_name[j])
                 for ic in range(1,c):
-                    ax[ic,i].scatter(t_hat,X[:,j,ic],c=cell_colors);
+                    ax[ic,i].scatter(t_hat,X[:,j,ic],c='gray')#c=cell_colors);
     
     ## plot Y
     Y_hat = np.zeros((L,n,p,2))
@@ -157,10 +158,10 @@ def plot_y(X,weight,traj,idx,gene_name=None,cell_colors=None):
         if len(idx)==1:
             i=idx[0]
             if c==1:
-                ax.scatter(t_hat,y_hat[:,i,0],c=t_hat,cmap=cmps[l])
+                ax.scatter(t_hat,y_hat[:,i,0],c=colors[l])
             else:
                 for ic in range(c):
-                    ax[ic].scatter(t_hat,y_hat[:,i,ic],c=t_hat,cmap=cmps[l]);
+                    ax[ic].scatter(t_hat,y_hat[:,i,ic],c=colors[l]);
         else:
             for i,j in enumerate(idx):
                 if c==1:
@@ -168,15 +169,15 @@ def plot_y(X,weight,traj,idx,gene_name=None,cell_colors=None):
 
                 else:
                     for ic in range(c):
-                        ax[ic,i].plot(t_hat,y_hat[:,j,ic],'k.');
-                          
-            
+                        ax[ic,i].scatter(t_hat,y_hat[:,j,ic],c=colors[l]);
+ 
+
 def plot_phase(X,weight,traj,idx,gene_name=None,cell_colors=None):
     theta_hat = traj.theta.copy()
     tau=traj.tau
     n,L,m=np.shape(weight)
     p=np.shape(traj.theta)[0]
-    h=np.linspace(0,tau[-1],m)
+    h=np.linspace(tau[0],tau[-1],m)
     t_hat=np.sum(weight[:,:,:]*h[None,None,:],axis=(1,2))
     if gene_name is None:
         gene_name = np.arange(p)
@@ -185,14 +186,14 @@ def plot_phase(X,weight,traj,idx,gene_name=None,cell_colors=None):
       
     fig, ax = plt.subplots(1,len(idx),figsize=(6*len(idx),4))
     for l in range(L):
-        t_hat=np.linspace(traj.tau[0],traj.tau[-1],traj.m)
+        t_hat=np.linspace(traj.tau[0],traj.tau[-1],100*traj.m)
         theta_l_hat = np.concatenate((theta_hat[:,traj.topo[l]], theta_hat[:,-4:]), axis=1)
         y_hat = traj.get_Y(theta_l_hat,t_hat,tau) # m*p*2
         if len(idx)==1:
             i=idx[0]
             ax.scatter(X[:,i,0]+np.random.normal(0,0.1,n),X[:,i,1]+np.random.normal(0,0.1,n),c=cell_colors,s=0.5,cmap='Greys');
             ax.scatter(y_hat[:,i,0],y_hat[:,i,1],c=t_hat,cmap=cmps[l]);
-            ax.set_title(gene_name[0])
+            ax.set_title(gene_name[i])
         else:
             for i,j in enumerate(idx):
                 ax[i].scatter(X[:,j,0]+np.random.normal(0,0.1,n),X[:,j,1]+np.random.normal(0,0.1,n),c=cell_colors,s=0.5,cmap='Greys');
